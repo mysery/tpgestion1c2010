bda3daeb7c64f8533f4d72b2d387df576b65dfa3
mysery <mysery@2315a462-10bc-0173-927f-a73b8dc12e8a>
2010-04-27 20:22:32 +0000

    * Unos fix en el algoritmo. * Mas intentos de mejorar el CostCalculator.

--- trunk/CostCalculator.cs
+++ trunk/CostCalculator.cs
@@ -19,9 +19,9 @@ namespace SolucionAlumno
 		*	Constante con el costo de moviento diagonal, multiplicado por 100 y truncado para no manerjar puntos flotantes. 
 		*/
         private const int DIAGONAL = 282;
-        
+
         private int totalCostCounted = 0;
-        private int totalCostSumed = 0;
+        private double totalCostSumed = 0;
 
 		/**
 		*	Instancia del singleton CostCalculator.
@@ -52,17 +52,18 @@ namespace SolucionAlumno
 		*/
         public int move(Node previousNode, Node actual, MapaDeCostos mapaDeCostos)
         {
-            int precalculed = previousNode.GValue + (int)Math.Truncate(100 * actual.Costo.Valor);
+            double costToFix = actual.Costo.Valor;//(int)Math.Truncate(1000 * actual.Costo.Valor);
+            totalCostSumed += costToFix;
+            totalCostCounted++;
+            int precalculed = previousNode.GValue;// +costToFix;
             if (previousNode.Point.X == actual.Point.X || previousNode.Point.Y == actual.Point.Y)
             {
-                precalculed += RECT;
+                precalculed += RECT + (int)Math.Truncate(100 * costToFix);
             } 
             else
             {
-                precalculed += DIAGONAL;
+                precalculed += DIAGONAL + (int)Math.Truncate(100 * costToFix);
             }
-            totalCostSumed += precalculed;
-            totalCostCounted++;
             return precalculed;
         }
         
@@ -72,13 +73,20 @@ namespace SolucionAlumno
         public int aproximateMove(Point actual, Point goal)
         {
             int value = 0;
+            int rectAproxCost = RECT + this.getAproximateCostTerain(); // + (int)Math.Truncate(RECT * totalCostSumed / totalCostCounted);
+            int diagonalAproxCost = DIAGONAL + this.getAproximateCostTerain(); // (int)Math.Truncate(DIAGONAL * totalCostSumed / totalCostCounted);
             int diagonal = Math.Min(Math.Abs(actual.X - goal.X), Math.Abs(actual.Y - goal.Y));
             int direct = Math.Abs(actual.X - goal.X) + Math.Abs(actual.Y - goal.Y);
-            value += DIAGONAL * diagonal + RECT * (direct - 2 * diagonal);
+            value += diagonalAproxCost * diagonal + rectAproxCost * (direct - 2 * diagonal);
+            //Distancia de Euclidean (Lenta por el SQRT)
             //value += (int)Math.Truncate(RECT * Math.Sqrt((Math.Pow((actual.X - goal.X), 2) + Math.Pow((actual.Y - goal.Y), 2))));
-            value += totalCostSumed / totalCostCounted;
+            //value = (value * (totalCostSumed / totalCostCounted))/100;
             return value;
         }
 
+        private int getAproximateCostTerain()
+        {
+            return 0;// (int)Math.Truncate(100 * totalCostSumed / totalCostCounted);
+        }
     }
 }
